fb9f17f46caa8ab0d620e4bab504ef24bbf22ed4
jlbuild <juliabuildbot@gmail.com>
2021-04-22 19:15:43 +0000

    ReadStat_jll build 1.1.5+0

--- src/ReadStat_jll.jl
+++ src/ReadStat_jll.jl
@@ -1,31 +1,9 @@
-module ReadStat_jll
-using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
-import Base: UUID
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule ReadStat_jll
+using Base
+using Base: UUID
+import JLLWrappers
 
-# We put these inter-JLL-package API values here so that they are always defined, even if there
-# is no underlying wrapper held within this JLL package.
-const PATH_list = String[]
-const LIBPATH_list = String[]
-
-# Load Artifacts.toml file
-artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
-
-# Extract all platforms
-artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("a4dc8951-f1cc-5499-9034-9ec1c3e64557"))
-platforms = [Pkg.Artifacts.unpack_platform(e, "ReadStat", artifacts_toml) for e in artifacts["ReadStat"]]
-
-# Filter platforms based on what wrappers we've generated on-disk
-platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
-
-# From the available options, choose the best platform
-best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
-
-# Silently fail if there's no binaries for this platform
-if best_platform === nothing
-    @debug("Unable to load ReadStat; unsupported platform $(triplet(platform_key_abi()))")
-else
-    # Load the appropriate wrapper
-    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
-end
-
+JLLWrappers.@generate_main_file_header("ReadStat")
+JLLWrappers.@generate_main_file("ReadStat", UUID("a4dc8951-f1cc-5499-9034-9ec1c3e64557"))
 end  # module ReadStat_jll
